de29905463bf09de3dacbd18b77502e4e5e73bf4
Jared Still <jkstill+github@gmail.com>
2017-01-21 22:55:38 -0800

    begin prototype p1.pl

--- p1.pl
+++ p1.pl
@@ -12,12 +12,12 @@ my $db = 'orcl';
 my $username = 'scott';
 my $password;
 my $maxSessions=2;
-my $exeDelay=.1; # seconds
-my $connectDelay=.25;
+my $exeDelay=0.1; # seconds
+my $connectDelay=0.25;
 my $connectMode='flood';
-my $exemode='sequential';
+my $exeMode='sequential';
 my $sqlDir='SQL';
-my $sqlFile='sqlfiles.conf'
+my $sqlFile='sqlfiles.conf';
 my $bindArraySize=1;
 my $cacheArraySize=100;
 
@@ -27,14 +27,14 @@ Getopt::Long::GetOptions(
 	"username=s" => \$username,
 	"password=s" => \$password,
 	"max-sessions=i" => \$maxSessions,
-	"exe-delay=f" => $exeDelay,
-	"connect-delay=f" => $connectDelay,
+	"exe-delay=f" => \$exeDelay,
+	"connect-delay=f" => \$connectDelay,
 	"exe-mode=s" => \$exeMode,
 	"sqldir=s" => \$sqlDir,
-	"sqlfile=s" => \$sqlFile ,
+	"sqlfile=s" => \$sqlFile,
 	"bind-array-size=i" => \$bindArraySize,
 	"cache-array-size=i" => \$cacheArraySize,
-	"sysdba!"s,
+	"sysdba!",
 	"sysoper!",
 	"z","h","help");
 
@@ -44,12 +44,12 @@ $connectionMode = 0;
 if ( $optctl{sysoper} ) { $connectionMode = 4 }
 if ( $optctl{sysdba} ) { $connectionMode = 2 }
 
-if ( ! defined($optctl{database}) ) {
+if ( ! defined($db) ) {
 	usage(1);
 }
 #$db=$optctl{database};
 
-if ( ! defined($optctl{username}) ) {
+if ( ! defined($username) ) {
 	usage(2);
 }
 
@@ -76,9 +76,9 @@ die "Connect to  $db failed \n" unless $dbh;
 # apparently not a database handle attribute
 # but IS a prepare handle attribute
 #$dbh->{ora_check_sql} = 0;
-$dbh->{RowCacheSize} = 100;
+$dbh->{RowCacheSize} = $cacheArraySize;
 
-my $sql=q{select user from dual};
+my $sql=q{select sys_context('userenv','sid') SID from dual};
 
 my $sth = $dbh->prepare($sql,{ora_check_sql => 0});
 
@@ -99,16 +99,48 @@ sub usage {
 	print qq/
 
 usage: $basename
+/;
 
-  -database      target instance
-  -username      target instance account name
-  -password      target instance account password
-  -sysdba        logon as sysdba
-  -sysoper       logon as sysoper
+print q/
+
+             --db  which database to connect to
+       --username  account to connect to
+       --password  obvious. 
+                   user will be prompted for password if not on the command line
+
+   --max-sessions  number of sessions to use
+
+      --exe-delay  seconds to delay between sql executions defaults to 0.1 seconds
+
+  --connect-delay  seconds to delay be between connections
+                   valid only for --session-mode trickle
+
+   --connect-mode  [ trickle | flood | tsunami ] - default is flood
+                   trickle: gradually add sessions up to max-sessions
+                   flood: startup sessions as quickly as possible
+                   tsunami: wait until all sessions are connected before they are allowed to work
+
+       --exe-mode  [ sequential | semi-random | truly-random ] - default is sequential
+                   sequential: each session iterates through the SQL statements serially
+                   semi-random: a value assigned in the sqlfile determines how frequently each SQL is executed
+                   truly-random: SQL selected randomly by each session
+
+         --sqldir  location of SQL script files and bind variable files. 
+                   default is .\/SQL
+
+        --sqlfile  this refers to the file that names the SQL script files to use 
+                   the names of the bind variable files will be defined here as well
+                   default is .\/sqlfile.conf
+
+--bind-array-size  defines how many records from the bind array file are to be used per SQL execution
+                   default is 1
+
+--cache-array-size defines the size of array to use to retreive data - similar to 'set array' in sqlplus 
+                   default is 100
 
   example:
 
-  $basename -database dv07 -username scott -password tiger -sysdba 
+  $basename -db dv07 -username scott -password tiger -sysdba 
 /;
    exit $exitVal;
 };
