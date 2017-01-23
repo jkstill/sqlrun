3352f7f8c6f6773698992d19fc20b57766adf7ce
Jared Still <jkstill+github@gmail.com>
2017-01-22 17:20:40 -0800

    renamed p1.pl sqlrun.pl

---
+++ sqlrun.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+./sqlrun.pl \
+	--exe-mode semi-random \
+	--connect-mode flood \
+	-db p1 \
+	-username jkstill \
+	-password grok \
+	--parmfile parameters.conf \
+	--sqlfile sqlfile.conf \
+	--debug 
+	#--timer-test
+
